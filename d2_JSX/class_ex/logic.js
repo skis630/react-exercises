6ed49497496c3fe86ca1ac48705a1305f672cd22
Sara <skis630@aucklanduni.ac.nz>
2019-06-24 14:03:09 +0300

    class star ex3

--- d2_JSX/class_ex/logic.js
+++ d2_JSX/class_ex/logic.js
@@ -1,12 +1,27 @@
 class Hello extends React.Component {
+    constructor() {
+        super();
+        this.counter = 0;
+        this.displayMessage = this.displayMessage.bind(this);
+    }
+
     greet() {
         let greeting = document.createElement("p");
         greeting.innerText = "Hello World!" 
         document.getElementById("root").append(greeting);
     }
+
+    displayMessage() {
+        this.counter++;
+        let message = `Hello ${this.counter}`;
+        document.querySelector('.message').innerHTML = message;
+        }        
     
     render() {
-        let btn = <button type="button" onClick={this.greet}>Click me!</button>;
+        let placeholder = document.createElement("p");
+        placeholder.setAttribute("class", "message");
+        document.getElementById("root").appendChild(placeholder);
+        let btn = <button type="button" onClick={this.displayMessage}>Click me!</button>;
 
         return btn;
     }
--- d2_JSX/ex3/logic.js
+++ d2_JSX/ex3/logic.js
@@ -1,4 +1,25 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class ShowNumbers extends React.Component {
+    constructor() {
+        super();
+        this.max = 100;
+        this.showNums = this.showNums.bind(this);
+    }
+
+    showNums() {
+        let num = Math.round(Math.random() * this.max);
+        let txtNode = document.createTextNode(`${num} `);
+        document.querySelector(".placeholder").appendChild(txtNode);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Numbers</h3>
+                <button type="button" onClick={this.showNums}>Show Numbers</button>
+                <div className="placeholder"></div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ShowNumbers/>, document.getElementById("root"));
