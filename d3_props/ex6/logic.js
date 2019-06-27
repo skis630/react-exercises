d89ef05ff080b6644238e63d72a08a0845314517
Sara <skis630@aucklanduni.ac.nz>
2019-06-27 20:30:33 +0300

    star ex3 - Box shape

--- d3_props/ex6/logic.js
+++ d3_props/ex6/logic.js
@@ -1,4 +1,35 @@
+class Box extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={this.props.size}></div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">{this.props.children}</div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App>
+            <Box size="xs"></Box>
+            <Box size="sm"></Box>
+            <Box size="md"></Box>
+            <Box size="sm"></Box>
+            <Box size="xs"></Box>
+            <Box size="lg"></Box>    
+    </App>,
     document.getElementById("root")
 );
