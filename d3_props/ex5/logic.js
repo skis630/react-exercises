b0b6a873f04b3dfe7432a1e0099e7e3ea86611da
Sara <skis630@aucklanduni.ac.nz>
2019-06-27 19:32:33 +0300

    star ex2 - services

--- d3_props/ex5/logic.js
+++ d3_props/ex5/logic.js
@@ -1,4 +1,82 @@
+class Stripe extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className={this.props.gp}>{this.props.children}</div>;
+    }
+}
+
+class Service extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="item">
+                <Icon icon={this.props.icon} />
+                <Desc desc={this.props.img} />
+            </div>
+        );
+    }
+}
+
+class Icon extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className={this.props.icon}></div>;
+    }
+}
+
+class Desc extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={this.props.desc} />
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Stripe gp="navbar navbar-default">
+                    <div class="container-fluid">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#"><img src="./images/navbar.PNG" /></a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><img src="./images/icon.PNG" width="40" heigh="40"/></a></li>
+                        </ul>
+                    </div>
+                </Stripe>
+                <Stripe gp="row main text-center">
+                    <img src="./images/main.PNG" className="img-responsive"/>
+                </Stripe>
+                <Stripe gp="row text-center">
+                    <Service icon="item1 text-center" img="./images/ui.PNG" />
+                    <Service icon="item2 text-center" img="./images/concept.PNG" />
+                    <Service icon="item3 text-center" img="./images/design.PNG"></Service>
+                </Stripe>
+                <Stripe gp="footer"></Stripe>
+            </div>           
+        );
+    }
+}
+
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App/>,
     document.getElementById("root")
 );
