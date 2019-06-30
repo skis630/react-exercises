f0891fb46bea9cfc62bb1b8c18705ecc10a67e7d
Sara <skis630@aucklanduni.ac.nz>
2019-06-30 17:02:54 +0300

    react state star ex2

--- d4_state/ex4/logic.js
+++ d4_state/ex4/logic.js
@@ -1,4 +1,46 @@
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabled: false
+        }
+        this.disable = this.disable.bind(this);
+    }
+
+    disable() {
+        this.setState({disabled: true});
+    }
+
+    render() {
+        return (
+            <button type="button" onClick={this.disable} disabled={this.state.disabled}>{this.props.color}</button>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="header"></div>
+                <div className="buttons">
+                    <Button color="blue" />
+                    <Button color="purple" />
+                    <Button color="green" />
+                    <Button color="red" />
+                    <Button color="pink" />
+                </div>
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App/>,
     document.getElementById("root")
 );
