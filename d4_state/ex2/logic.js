8a58efb6cd5d2168ec152719e8a720c9d3c175bf
Sara <skis630@aucklanduni.ac.nz>
2019-06-30 14:01:34 +0300

    react state class star ex2

--- d4_state/ex2/logic.js
+++ d4_state/ex2/logic.js
@@ -1,4 +1,31 @@
+class Box extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibility: "show"
+        };
+        this.toggleShow = this.toggleShow.bind(this);
+    }
+
+    toggleShow() {
+        let newState = this.state.visibility === "hidden" ? "show" : "hidden";
+        this.setState({
+            visibility: newState
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <button type="button" onClick={this.toggleShow}>show/hide</button>
+                <div className={this.state.visibility}></div>
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <Box />,
     document.getElementById("root")
 );
