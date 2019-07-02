0494f23d1c0afad4b796ad3e6d89f962cfd05fb5
Sara <skis630@aucklanduni.ac.nz>
2019-07-02 11:51:18 +0300

    react dataflow class star - boxes

---
+++ d6_data_flow/ex2/logic.js
@@ -0,0 +1,60 @@
+class Box extends React.Component {
+    constructor(props) {
+        super(props);       
+    }
+
+    handleClick(e) {
+        this.props.handleClick(e);
+    }
+
+    render() {
+        return (         
+                <div className={this.props.box} onClick={e => this.handleClick(e)}>{this.props.children}</div>           
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            frame: "transparent"
+        }
+    }
+
+    changeColor(e) {
+        let newFrame = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
+        
+        this.setState({
+            frame: newFrame
+        });
+    }
+
+    render() {
+        let style = {
+            backgroundColor: this.state.frame,
+            border: "1px solid black",
+            padding: "20px",
+            width: "440px"
+        }
+        return (
+            <div style={style} className="frame">
+                <Box box="green" handleClick={e => this.changeColor(e)}>
+                    <Box box="blue" handleClick={e => this.changeColor(e)}>
+                        <Box box="pink" handleClick={e => this.changeColor(e)}>
+                            <Box box="purple" handleClick={e => this.changeColor(e)} />
+                            <Box box="purple" handleClick={e => this.changeColor(e)} />
+                        </Box>
+                    </Box>
+                </Box>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
+
